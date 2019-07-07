a96d2929123f0b629a56465b8c0167c6fbe6378c
岡本憲聖 <okamotokensei@okamotokenshounoMacBook-puro.local>
2019-07-07 19:25:34 +0900

    [Start][Finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,14 @@ class BlogsController < ApplicationController
 		redirect_to blogs_path
   end
 
-  def edit
+	def edit
+		@blog = Blog.find(params[:id])
+	end
+
+	def update
+		blog = Blog.find(params[:id])
+		blog.update(blog_params)
+		redirect_to blog_path(blog)
 	end
 
 	private
