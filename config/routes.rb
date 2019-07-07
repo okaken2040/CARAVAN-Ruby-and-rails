c0283733497556ee73fabd79c22d3f1f4d86e7dd
岡本憲聖 <okamotokensei@okamotokenshounoMacBook-puro.local>
2019-07-07 17:04:45 +0900

    [Start][Finish]4-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'blogs/show'
+  get 'blogs/index'
+  get 'blogs/new'
+  get 'blogs/edit'
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :blogs
 end
